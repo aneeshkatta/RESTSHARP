f0242aee1faa8aa5bb62f4042a1238b42848d185
Aneesh <katta.aneesh597@gmail.com>
2023-01-18 09:07:50 +0530

    added program for Ability to Retrieve all Employees in EmployeePayroll JSON Server

---
+++ TestProject1RestSharp/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1RestSharp
+{
+    internal class Employees
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+    }
+}
